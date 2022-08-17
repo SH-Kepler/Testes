6d724824fe10ae586632e26b39fadf19f1e238b0
Sheifer Kepler <sheifer.kda@gmail.com>
2022-08-17 16:48:09 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testando o componente App.js', () => {
+  test('teste se é exibida a mensagem "No favorite pokemon"', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const empty = screen.getByText(/No favorite pokemon found/i);
+    expect(empty).toBeInTheDocument();
+  });
+  test('Teste se são exibidos todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+
+    const moreDetails = screen.getByText(/More details/i);
+    userEvent.click(moreDetails);
+    const checkFavorite = screen.getByRole('checkbox', { id: 'favorite' });
+    userEvent.click(checkFavorite);
+    const favLink = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    userEvent.click(favLink);
+    const pokemon = screen.getByTestId('pokemon-name');
+    expect(pokemon).toBeInTheDocument();
+  });
+});
